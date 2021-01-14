2730de61f61a86b4ebab88ff4b7cc1078f60bdee
Dmitry_Meshkov <dm_91@bk.ru>
2021-01-14 15:01:05 +0300

    Развитие логики игры

--- AsteroidGame/Game.cs
+++ AsteroidGame/Game.cs
@@ -9,6 +9,7 @@ namespace AsteroidGame
         private static BufferedGraphicsContext __Context;
         private static BufferedGraphics __Buffer;
 
+        private static VisualObject[] __GameObjects;
         public static int Width { get; set; }
         public static int Height { get; set; }
 
@@ -22,17 +23,39 @@ namespace AsteroidGame
             __Buffer = __Context.Allocate(g, new Rectangle(0, 0, Width, Height));
         }
 
+        public static void Load()
+        {
+            const int visual_objects_count = 30;
+            __GameObjects = new VisualObject[visual_objects_count];
+
+            for(int i = 0; i < __GameObjects.Length; i++)
+            {
+                __GameObjects[i] = new VisualObject(
+                    new Point(600, i * 20),
+                    new Point(15 - i, 20 - i),
+                    new Size(20, 20));
+            }
+        }
+
         public static void Draw()
         {
             Graphics g = __Buffer.Graphics;
 
             g.Clear(Color.Black);
 
-            g.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            //g.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            //g.FillEllipse(Brushes.Red, new Rectangle(100, 100, 200, 200));
 
-            g.FillEllipse(Brushes.Red, new Rectangle(100, 100, 200, 200));
+            foreach (var game_object in __GameObjects)
+                game_object.Draw(g);
 
             __Buffer.Render();
+        }
+
+        private static void Update()
+        {
+            foreach (var game_object in __GameObjects)
+                game_object.Update();
         }
     }
 }
