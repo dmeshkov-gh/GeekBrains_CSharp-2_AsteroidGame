c2bf6cc34ee31ad934c9f06d6b8f6231a0d71abc
Dmitry_Meshkov <dm_91@bk.ru>
2021-01-18 15:05:06 +0300

    Создан класс GameObjectException

---
+++ AsteroidGame/GameObjectException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidGame
+{
+    class GameObjectException : Exception
+    {
+        private string MessageDetails { get; set; }
+        public DateTime ErrorTime { get; private set; }
+
+        public GameObjectException(string message)
+        {
+            MessageDetails = message;
+            ErrorTime = DateTime.Now;
+        }
+
+        public override string Message => $"[{ErrorTime}]: {MessageDetails}.";
+    }
+}
