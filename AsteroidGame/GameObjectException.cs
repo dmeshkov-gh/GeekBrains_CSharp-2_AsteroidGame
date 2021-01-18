866a3704bb49a55cffa017461ff25df63f86fd6d
Dmitry_Meshkov <dm_91@bk.ru>
2021-01-18 14:23:44 +0300

    Класс GameObjectException

---
+++ AsteroidGame/GameObjectException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidGame
+{
+    class GameObjectException : Exception
+    {
+        private string MessageDetails { get; set; }
+        public DateTime ErrorTime { get; private set; }
+
+        public GameObjectException(string message)
+        {
+            MessageDetails = message;
+        }
+
+        public override string Message => $"[{ErrorTime}]: {MessageDetails}. \nMember name of exception: {TargetSite}";
+    }
+}
